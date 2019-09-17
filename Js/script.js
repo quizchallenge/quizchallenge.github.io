6d71025c24884a14d75614400169cea8a8d38d13
quizchallenge <55434738+quizchallenge@users.noreply.github.com>
2019-09-17 16:20:56 +0530

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,86 @@
+function myFunctionGreen1()
+{
+   
+    document.getElementById("box-1").style.backgroundColor = "green";
+    var x = document.getElementById("audio");
+    x.autoplay = true;
+    x.load();
+
+}
+
+function myFunctionGreen2()
+{
+   
+    document.getElementById("box-1").style.backgroundColor = "green";
+    var x = document.getElementById("audio");
+    x.autoplay = true;
+    x.load();
+
+}
+
+function myFunctionGreen3()
+{
+   
+    document.getElementById("box-1").style.backgroundColor = "green";
+    var x = document.getElementById("audio");
+    x.autoplay = true;
+    x.load();
+
+}
+
+function myFunctionGreen4()
+{
+   
+    document.getElementById("box-1").style.backgroundColor = "green";
+    var x = document.getElementById("audio");
+    x.autoplay = true;
+    x.load();
+
+}
+
+function myFunctionRed1()
+{
+   
+    document.getElementById("box-2").style.backgroundColor = "red";
+    var x = document.getElementById("audio-2");
+    x.autoplay = true;
+    x.load();
+
+
+}
+
+function myFunctionRed2()
+{
+   
+    document.getElementById("box-2").style.backgroundColor = "red";
+    var x = document.getElementById("audio-2");
+    x.autoplay = true;
+    x.load();
+
+
+}
+
+function myFunctionRed3()
+{
+   
+    document.getElementById("box-3").style.backgroundColor = "red";
+    var x = document.getElementById("audio-2");
+    x.autoplay = true;
+    x.load();
+
+
+}
+
+function myFunctionRed4()
+{
+   
+    document.getElementById("box-4").style.backgroundColor = "red";
+    var x = document.getElementById("audio-2");
+    x.autoplay = true;
+    x.load();
+
+
+}
+
+
+    
